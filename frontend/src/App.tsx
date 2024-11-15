20dbb67e5f03bbcc6ff295b3b10c2b0fc7e4c3eb
Khoa Nguyen <akhoa.nv@gmail.com>
2024-11-14 23:59:26 -0500

    Added home screen

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,36 +4,42 @@ import Sidebar from "./components/SideBar"
 import SummaryIcon from "./assets/summary.png"
 import SettingIcon from "./assets/setting.png"
 import ReadAloudIcon from "./assets/read-aloud.png"
-import SummaryScreen from "./screen/SummaryScreen.tsx"
-import ReadaloudScreen from "./screen/ReadaloudScreen.tsx"
-import SettingScreen from "./screen/SettingScreen.tsx"
-import HomeScreen from "./screen/HomeScreen.tsx"
+import SummaryScreen from "./screen/SummaryScreen"
+import ReadaloudScreen from "./screen/ReadaloudScreen"
+import SettingScreen from "./screen/SettingScreen"
+import HomeScreen from "./screen/HomeScreen"
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation} from "react-router-dom";
 import { useEffect } from "react";
 
-const App: React.FC = () => {
+function AppLayout() {
   const sidebarParams = [
     {imageSrc: SummaryIcon, title: "Summary", path: "/summary"},
     {imageSrc: ReadAloudIcon, title: "Read Aloud", path: "/readaloud"},
     {imageSrc: SettingIcon, title: "Settings", path: "/setting"},
   ];
 
+  const hideSidebar = useLocation().pathname === "/";
+
   return (
-    <Router>
-        <div style={{ display: "flex" }}>
-          <Sidebar params={sidebarParams} />
-          <div style={{marginLeft: "90px", padding: "20px"}}>
-            <Routes>
-                <Route path="/" element={<HomeScreen />} />
-                <Route path="/summary" element={<SummaryScreen />} />
-                <Route path="/readaloud" element={<ReadaloudScreen />} />
-                <Route path="/setting" element={<SettingScreen />} />
-            </Routes>
-          </div>
-        </div>
-    </Router>
+    <div>
+      {!hideSidebar && <Sidebar params={sidebarParams} />}
+      <div style={{marginLeft: "80px"}}>
+        <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/summary" element={<SummaryScreen />} />
+            <Route path="/readaloud" element={<ReadaloudScreen />} />
+            <Route path="/setting" element={<SettingScreen />} />
+        </Routes>
+      </div>
+    </div>
   );
 };
 
-export default App;
+export default function App() {
+    return (
+        <Router>
+            <AppLayout />
+        </Router>
+    );
+}
--- frontend/src/components/SideBar.tsx
+++ frontend/src/components/SideBar.tsx
@@ -11,11 +11,11 @@ const Sidebar: React.FC<SidebarProps> = ({params}) => {
   return (
     <div className="sidebar">
         <div id="icon-holder">
-            <img id="icon" src={Icon} />
+            <img id="sidebar-icon" className="icon" src={Icon} />
         </div>
         {params.map((params, index) => (
-            <Link className="nav-link" to={params.path}>
-                <div key={"nav-cont-icon-" + index} className="nav-container" onClick={params.onClick}>
+            <Link key={"nav-btn-link-" + index} className="link" to={params.path}>
+                <div key={"nav-btn-icon-" + index} className="nav-container" onClick={params.onClick}>
                     <img key={"nav-btn-icon-" + index} className="nav-icon" src={params.imageSrc} />
                     <p key={"nav-btn-title-" + index} className="nav-title"> {params.title} </p>
                 </div>
