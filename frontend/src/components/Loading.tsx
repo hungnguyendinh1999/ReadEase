df7eb71e9c98c8a3869aae648e936d6059a10097
Khoa Nguyen <akhoa.nv@gmail.com>
2024-11-16 00:11:08 -0500

    Added new text-to-speech-feature

---
+++ frontend/src/components/Loading.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Loading.css"
+import IconWhite from "../assets/icon-white.png"
+
+interface LoadingProps {
+    size: number;
+}
+
+const Loading: React.FC<LoadingProps> = ({size}) => {
+    return (
+        <div id="loading-container" style={{height: size, width: size}}>
+            <img src={IconWhite} id="loading-img" />
+        </div>
+    );
+};
+
+export default Loading;
