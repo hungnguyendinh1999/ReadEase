5ede3a46aa715fb488efa26771c439048f48bae7
Hung Dinh Nguyen <hungnguyendinh1999@gmail.com>
2024-11-16 20:24:22 -0500

    minor changes to UI

--- frontend/src/screen/SummaryScreen.tsx
+++ frontend/src/screen/SummaryScreen.tsx
@@ -1,4 +1,4 @@
-import React, {useState, ChangeEvent, FC} from "react";
+import React, { useState, ChangeEvent, FC } from "react";
 import "./SummaryScreen.css";
 import TextBox from "../components/atom/TextBox";
 import UploadFileButton from "../components/molecules/UploadFileButton";
@@ -9,6 +9,8 @@ const SummaryScreen: FC = () => {
     // Define state with types
     const [text, setText] = useState<string>("");
     const [vocabLevel, setVocabLevel] = useState<string>("Default");
+    const [isSubmitted, setIsSubmitted] = useState<boolean>(false);
+    const [height, setHeight] = useState<string>("80%");
 
     // Event handler for textarea input
     const handleTextChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
@@ -29,22 +31,32 @@ const SummaryScreen: FC = () => {
     const handleSubmit = () => {
         // Submit logic will go here
         console.log("handleSubmit() called");
+        setIsSubmitted(true); // Trigger animations and state changes
+        setHeight("10%");
         console.log("sending " + text);
     };
 
     return (
         <div id="summary-screen">
-            <div id="flex-box">
-                <div id="summary-container">
-                    <div id="summary-textbox-container">
+            <div className={`flex-box ${isSubmitted ? "shrink-outer" : ""}`}>
+                <div id="summary-input-container" style={{ height: height }}>
+                    <div
+                        id="summary-textbox-container"
+                        className={`${isSubmitted ? "shrink" : ""}`}
+                    >
                         {/* Use custom TextBox component */}
-                        <TextBox value={text} onChange={handleTextChange}
+                        <TextBox
+                            value={text}
+                            onChange={handleTextChange}
                             placeholder="Type text here, or upload a txt file"
                         />
                     </div>
 
                     {/* Button Group */}
-                    <div id="summary-button-group">
+                    <div
+                        id="summary-button-group"
+                        className={`${isSubmitted ? "hidden" : ""}`}
+                    >
                         {/* Left aligned button */}
                         <div id="summary-left-buttons">
                             <UploadFileButton
@@ -55,14 +67,42 @@ const SummaryScreen: FC = () => {
 
                         {/* Right aligned button */}
                         <div id="summary-right-buttons">
-                            <Dropdown options={["Default", "ELI5", "Intermediate", "Advanced"]}
-                                value={vocabLevel} onChange={handleVocabLevelChange}
+                            <Dropdown
+                                options={[
+                                    "Default",
+                                    "ELI5",
+                                    "Intermediate",
+                                    "Advanced",
+                                ]}
+                                value={vocabLevel}
+                                onChange={handleVocabLevelChange}
                             />
-                            <SubmitButton label="" onClick={handleSubmit}/>
+                            <SubmitButton label="" onClick={handleSubmit} />
                         </div>
                     </div>
                 </div>
             </div>
+            {isSubmitted && (
+                <div className="flex-box">
+                    <div id="summary-output-container">
+                        <div>
+                            <div id="summary-title" className="center-text disable-selection">
+                                <h1>Summarized Text</h1>
+                            </div>
+                            <div>
+                                
+                            </div>
+                        </div>
+                        <div id="smaller-container" className="hide-caret">
+                            <TextBox
+                                value=""
+                                onChange={() => {}}
+                                placeholder="Enter output here..."
+                            />
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
